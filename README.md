f053633a60de282f4d9fb1fcfed067a61d6cd794
Stepan Dordiai <stepandordiai@gmail.com>
2025-06-27 11:24:52 +0200

    Update styles and info

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tasty Burger
+
+<img src="./public/screenshot/tasty-burger-ss-v2-c.png" alt="">
--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 setTimeout(() => {
-    document
-        .querySelector(".info-container")
-        .classList.add("info-container--attention");
+	document
+		.querySelector(".info-container")
+		.classList.add("info-container--attention");
 }, 3000);
 
 function getAccess() {
-    document
-        .querySelector(".info-container")
-        .classList.remove("info-container--attention");
+	document
+		.querySelector(".info-container")
+		.classList.remove("info-container--attention");
 }
 
 document.querySelector(".js-access-btn").addEventListener("click", () => {
-    getAccess();
+	getAccess();
 });
 
 document.querySelector(".current-year").innerHTML = new Date().getFullYear();
