a0d58f891dcd26fa5a203e9a01bf76552f68cac6
Stepan Dordiai <stepandordiai@gmail.com>
2025-07-28 15:50:27 +0200

    Update info

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-setTimeout(() => {
-	document
-		.querySelector(".info-container")
-		.classList.add("info-container--attention");
-}, 3000);
-
-function getAccess() {
-	document
-		.querySelector(".info-container")
-		.classList.remove("info-container--attention");
-}
-
-document.querySelector(".js-access-btn").addEventListener("click", () => {
-	getAccess();
-});
-
 document.querySelector(".current-year").innerHTML = new Date().getFullYear();
